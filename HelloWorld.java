dc9785abffacc0565139f8bba8e541655e0d1536
feeiang <feeiang@gmail.com>
2021-12-11 22:16:28 -0600

    Update HelloWorld.java
    Add a 2nd line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World! \nThis is a Java app for DevOps Project 3.");
+    System.out.println("2nd line to trigger Jenkins Docker CI/CD Pipeline");
   }
 }
 
